a69dd179b558af1565859849780a807403bbf8b8
Matthew Ntsiful <matthew.ntsiful@gmail.com>
2025-08-13 00:57:31 +0000

    update staging environment configuration

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source               = "git::https://github.com/matthewntsiful/terraform-aws-modules.git//modules/vpc?ref=main"
   name_prefix          = "BlakkBrotherInc"
-  environment          = "staging"
+  environment          = ["staging"]
   region               = "us-east-1"
   vpc_cidr             = "10.1.0.0/16"
   subnet_cidr          = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24", "10.1.4.0/24"]
@@ -17,7 +17,7 @@ module "security-group" {
   source = "git::https://github.com/matthewntsiful/terraform-aws-modules.git//modules/security-group?ref=main"
 
   name_prefix                = "BlakkBrotherInc"
-  environment                = "staging"
+  environment                = ["staging"]
   region                     = "us-east-1"
   vpc_id                     = module.vpc.vpc_id
   security_group_name_suffix = "web-sg"
@@ -38,15 +38,17 @@ module "ec2" {
   source = "git::https://github.com/matthewntsiful/terraform-aws-modules.git//modules/ec2?ref=main"
 
   name_prefix       = "BlakkBrotherInc"
-  environment       = "staging"
+  environment       = ["staging"]
   region            = "us-east-1"
   subnet_id         = module.vpc.public_subnet_ids[0]
   security_group_id = module.security-group.security_group_id
   created_by        = "Matthew Ntsiful"
   managed_by        = "Terraform"
 
-  # Staging-specific instance type
-  instance_type = "t3.small"
+  # Instance type mapping by environment
+  instance_type = {
+    staging = "t3.small"
+  }
 
   # Custom SSH port (matching security group)
   custom_ssh_port = 69
