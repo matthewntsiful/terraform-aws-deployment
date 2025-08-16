75d9bb471c00dff34f9acb4a585803bdd7f14b56
Matthew Ntsiful <matthew.ntsiful@gmail.com>
2025-08-16 11:58:26 +0000

    Infrastructure Deployment

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,8 +3,8 @@ terraform {
     bucket         = "deployments-dev-state"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    #dynamodb_table = "deployments-dev-locks"
-    use_lockfile = true
+    dynamodb_table = "deployments-dev-locks"
+    #use_lockfile = true
     encrypt        = true
   }
 }
